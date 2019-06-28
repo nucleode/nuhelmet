35a894b7b378ddf6909a3cd60ef9a0ab7883bb22
Matteo Vivona <kapa89@me.com>
2019-06-28 17:46:57 +0200

    Import files

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import fastifyPlugin from 'fastify-plugin'
+import helmet from 'fastify-helmet'
+
+export default fastifyPlugin(function(fastify, opts, next) {
+  const { domain } = opts
+  fastify.register(helmet, {
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'", domain],
+        styleSrc: ["'self'", domain, "'unsafe-inline'", 'fonts.googleapis.com'],
+        fontSrc: ["'self' data:", 'fonts.googleapis.com', 'fonts.gstatic.com'],
+        scriptSrc: ["'self'", domain].concat(
+          process.env.NODE_ENV === 'development' ? ["'unsafe-eval'", "'unsafe-inline'"] : [],
+        ),
+        objectSrc: ["'none'"],
+      },
+    },
+  })
+  next()
+})
